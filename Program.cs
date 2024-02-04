9f6e7172c3132f88f2d3bcc465e3add5cf7c950c
unknown <namitlodh>
2024-02-05 00:33:32 +0530

    [Namit] Add: Operations on Dictionary

--- Program.cs
+++ Program.cs
@@ -6,43 +6,117 @@ class Program
 {
     static void Main()
     {
-        // Creating a HashSet
-        HashSet<int> myHashSet = new HashSet<int>();
+        // Create a new dictionary of strings, with string keys.
+        //
+        Dictionary<string, string> openWith =
+            new Dictionary<string, string>();
 
-        // Adding elements to the HashSet
-        myHashSet.Add(1);
-        myHashSet.Add(2);
-        myHashSet.Add(3);
+        // Add some elements to the dictionary. There are no
+        // duplicate keys, but some of the values are duplicates.
+        openWith.Add("txt", "notepad");
+        openWith.Add("bmp", "paint");
+        openWith.Add("dib", "paint");
+        openWith.Add("rtf", "wordpad");
 
-        // Displaying the elements in the HashSet
-        DisplayHashSet(myHashSet);
-
-        // Checking if an element is present
-        int searchElement = 2;
-        Console.WriteLine($"Is {searchElement} present in the HashSet? {myHashSet.Contains(searchElement)}");
-
-        // Removing an element
-        myHashSet.Remove(2);
-
-        // Displaying the updated HashSet
-        Console.WriteLine("\nHashSet after removing element 2:");
-        DisplayHashSet(myHashSet);
-
-        // Clearing the HashSet
-        myHashSet.Clear();
-
-        // Checking if the HashSet is empty
-        Console.WriteLine("\nIs the HashSet empty? " + (myHashSet.Count == 0));
-    }
-
-    static void DisplayHashSet(HashSet<int> hashSet)
-    {
-        Console.WriteLine("Elements in the HashSet:");
-        foreach (var item in hashSet)
+        // The Add method throws an exception if the new key is
+        // already in the dictionary.
+        try
         {
-            Console.Write(item + " ");
+            openWith.Add("txt", "winword");
         }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("An element with Key = \"txt\" already exists.");
+        }
+
+        // The Item property is another name for the indexer, so you
+        // can omit its name when accessing elements.
+        Console.WriteLine("For key = \"rtf\", value = {0}.",
+            openWith["rtf"]);
+
+        // The indexer can be used to change the value associated
+        // with a key.
+        openWith["rtf"] = "winword";
+        Console.WriteLine("For key = \"rtf\", value = {0}.",
+            openWith["rtf"]);
+
+        // If a key does not exist, setting the indexer for that key
+        // adds a new key/value pair.
+        openWith["doc"] = "winword";
+
+        // The indexer throws an exception if the requested key is
+        // not in the dictionary.
+        try
+        {
+            Console.WriteLine("For key = \"tif\", value = {0}.",
+                openWith["tif"]);
+        }
+        catch (KeyNotFoundException)
+        {
+            Console.WriteLine("Key = \"tif\" is not found.");
+        }
+
+        
+        string value = "";
+        if (openWith.TryGetValue("tif", out value))
+        {
+            Console.WriteLine("For key = \"tif\", value = {0}.", value);
+        }
+        else
+        {
+            Console.WriteLine("Key = \"tif\" is not found.");
+        }
+
+        // ContainsKey can be used to test keys before inserting
+        // them.
+        if (!openWith.ContainsKey("ht"))
+        {
+            openWith.Add("ht", "hypertrm");
+            Console.WriteLine("Value added for key = \"ht\": {0}",
+                openWith["ht"]);
+        }
+
+        // When you use foreach to enumerate dictionary elements,
+        // the elements are retrieved as KeyValuePair objects.
         Console.WriteLine();
+        foreach (KeyValuePair<string, string> kvp in openWith)
+        {
+            Console.WriteLine("Key = {0}, Value = {1}",
+                kvp.Key, kvp.Value);
+        }
+
+        // To get the values alone, use the Values property.
+        Dictionary<string, string>.ValueCollection valueColl =
+            openWith.Values;
+
+        // The elements of the ValueCollection are strongly typed
+        // with the type that was specified for dictionary values.
+        Console.WriteLine();
+        foreach (string s in valueColl)
+        {
+            Console.WriteLine("Value = {0}", s);
+        }
+
+        // To get the keys alone, use the Keys property.
+        Dictionary<string, string>.KeyCollection keyColl =
+            openWith.Keys;
+
+        // The elements of the KeyCollection are strongly typed
+        // with the type that was specified for dictionary keys.
+        Console.WriteLine();
+        foreach (string s in keyColl)
+        {
+            Console.WriteLine("Key = {0}", s);
+        }
+
+        // Use the Remove method to remove a key/value pair.
+        Console.WriteLine("\nRemove(\"doc\")");
+        openWith.Remove("doc");
+
+        if (!openWith.ContainsKey("doc"))
+        {
+            Console.WriteLine("Key \"doc\" is not found.");
+        }
         Console.ReadLine();
     }
 }
